e76d8f3086ddfbd88e5c9743cc795ef1cd514eda
PM-Vielma <pablom.vielma1130@gmail.com>
2018-03-17 16:41:25 -0500

    Added a resource with a tiny bit of backend that can store entries

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     
     get 'verifier', to: 'verifier#index'
     get 'verify', to: 'verifier#show'
+
+    resources :entries
     # resources :verifier, only:[:index, :show]
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180317202706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entries", force: :cascade do |t|
+    t.string "premise"
+    t.string "conclusion"
+    t.string "proof"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
