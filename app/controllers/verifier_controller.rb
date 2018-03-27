23317d8b52a6e00cdfd9181e3f2874e50c24511f
PM-Vielma <pablom.vielma1130@gmail.com>
2018-03-26 21:51:25 -0500

    Updated UI

--- app/controllers/verifier_controller.rb
+++ app/controllers/verifier_controller.rb
@@ -9,8 +9,4 @@ class VerifierController < ApplicationController
         params = {'premesis' => 'PvQ->R, P, @xFx, Fa&R->S', 'conclusion' => 'S', 'proof' => '1       (1) PvQ->R    A\n2       (2) P         A\n2       (3) PvQ       2 vI\n1,2     (4) R         1,3 ->E\n5       (5) @xFx      A\n5       (6) Fa        5 @E\n1,2,5   (7) Fa&R      4,6 &I\n8       (8) Fa&R->S   A\n1,2,8,5 (9) S         7,8 ->E', '.cgifields' => 'primitives_only'}
         @x = Net::HTTP.post_form(URI.parse('http://logic.tamu.edu/daemon.html'), params)
     end
-
-	def create
-	  render plain: params[:proof].inspect
-	end
 end
