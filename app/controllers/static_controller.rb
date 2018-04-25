e52b302475b6fda85bda73342880e88d5e24f56e
PM-Vielma <pablom.vielma1130@gmail.com>
2018-04-24 20:59:29 -0500

    need help with quiz view for taking in multiple inputs. outputs work fine per question

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,15 +2,6 @@ class StaticController < ApplicationController
   def quiz
 	@questions = Question.all
 
-	i = 0
-  	@questions.each do |question|
-  		if i == 0
-	  		question.response = "yes"
-  		else
-  			question.response = "no"
-		end
-  		i = i + 1
-	end
 
   end
 end
