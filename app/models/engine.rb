9d4895ac4be6a87983049409e8c752c91136f332
fghjconner <fghjconner@gmail.com>
2018-04-30 22:06:52 +0000

    One indexed logic error message

--- app/models/engine.rb
+++ app/models/engine.rb
@@ -281,7 +281,7 @@ module Engine
 
 		proof.each_with_index { |line, i|
 			unless line.valid?
-				raise ProofError.new("Invalid proof, line #{i} does not follow", proof_string.lines[i])
+				raise ProofError.new("Invalid proof, line #{i+1} does not follow", proof_string.lines[i])
 			end
 		}
 		return false unless assumptions <= premesis
